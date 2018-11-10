601b1c6a6e69809121065955763abbbb5eea81a1
y-murakami0502 <y-murakami@nekojarashi.com>
2018-11-10 12:36:58 +0000

    Complete 7.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 保存の成功
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # redirect_to user_url(@user)
     else
       render 'new'
     end
